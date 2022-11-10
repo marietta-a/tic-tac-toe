220d4aa42e5f96e9e8e5ee473051a55d1cc7d780
akumbom <akumbom5ma@gmail.com>
2022-11-10 11:02:49 +0100

    fix flex styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import MediaQuery from "react-responsive";
 
 // class ShoppingList extends React.Component {
 //     render(){
